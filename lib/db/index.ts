bb0a6c4245b1697b24cf72e1ae01a5d3c215d0ec
Ivaylo Gugalov <ivaylogugalov@gmail.com>
2024-06-23 19:06:31 +0300

    fixing build errors

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -7,12 +7,10 @@ const isDevelopment = env.NODE_ENV === 'development'
 neonConfig.fetchConnectionCache = true
 
 export const sql = neon(env.DATABASE_URL)
-// @ts-expect-error
 export const db = drizzle(sql, { logger: isDevelopment })
 export const pool = new Pool({ connectionString: env.DATABASE_URL })
 
 
 export const sql_log = neon(env.DATABASE_APP_LOGS_URL)
-// @ts-expect-error
 export const db_log = drizzle(sql_log)
 export const pool_log = new Pool({ connectionString: env.DATABASE_APP_LOGS_URL })
